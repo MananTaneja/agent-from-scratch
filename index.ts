70f23b2383c932d09ccdccb1c828e003807d7faf
MananTaneja <manantaneja007@gmail.com>
2025-03-03 20:13:12 +0530

    feat: init api call logic

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config'
+import { runLlm } from './src/llm'
 
 const userMessage = process.argv[2]
 
@@ -6,3 +7,6 @@ if (!userMessage) {
   console.error('Please provide a message')
   process.exit(1)
 }
+
+const response = await runLlm({ userMessage })
+console.log(response)
